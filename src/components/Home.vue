3472afe22c265377d93c6c8162c2b103dbb37e2c
toriikatsuya2019 <toriikatsuya0707@gmail.com>
2019-05-20 13:59:01 +0900

    jsonデータ受け渡しまで

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+    <time-line :timelines="timelines"></time-line>
+</template>
+
+<script>
+import Vue from 'vue';
+import TimeLine from './TimeLine'
+import data from '../assets/data.json'
+
+export default {
+  components: {
+    TimeLine
+  },
+  data () {
+    return {
+      timelines: data.timelines
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
